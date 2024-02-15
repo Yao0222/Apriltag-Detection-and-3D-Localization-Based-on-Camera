4699a35b093a323626c32bb951e854ce3d6c03a9
Yao <154312757+Yao0222@users.noreply.github.com>
2024-02-15 11:12:17 -0500

    Update data_collect.sh

--- data_collect.sh
+++ data_collect.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 record_time=15 # set time
-filename="capture.pcap" # tcpdump_save
+
+# Create a new folder with timestamp
+folder_name=$(date +%Y%m%d_%H%M%S) # e.g., 20240215_123456
+mkdir -p "/home/nuc/Documents/mmwave/captures/$folder_name"
+
+# Update filename to include the new folder path
+filename="/home/nuc/Documents/mmwave/captures/$folder_name/capture.pcap" # tcpdump_save
 
 source /home/nuc/miniconda3/etc/profile.d/conda.sh
 conda activate mmwave
@@ -9,7 +15,7 @@ conda activate mmwave
 # run python script
 python /home/nuc/Documents/mmwave/feb12-main/test_mmwave.py &
 
-# run tcpdump
+# run tcpdump and save to the new folder
 sudo timeout ${record_time} tcpdump -i enp89s0 -w ${filename} -B 524288
 
 # optional: deactivate the conda environment
